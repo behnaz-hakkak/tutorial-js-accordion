baf968ed7dad01cdee6d777a07c9c6a6c0754f6f
imansa <i.s.azizy@gmail.com>
2019-05-02 12:59:34 +0430

    create JSAccordion class with constructor and instances management with sample

---
+++ src/js/jsaccordion.js
@@ -0,0 +1,33 @@
+function JSAccordion(elementOrSelector) {
+    if(!(this instanceof JSAccordion))
+        return new JSAccordion(elementOrSelector);
+
+    //  define public methods
+    this.test = function(color) {
+        this.targetElement.style.color = color;
+    };
+
+    //  start construction operations
+    //  if parameter is element selector
+    if(typeof elementOrSelector == 'string') {
+        this.targetElement = document.querySelector(elementOrSelector);
+        if(this.targetElement == null) {
+            throw ('invalid element selector');
+        }
+    }
+    //  if parameter is element DOM object
+    else if(typeof elementOrSelector == 'object')
+        this.targetElement = elementOrSelector;
+    else
+        throw ('Unknown element type');
+
+    //  set autoincrement instance id to object
+    this.id = JSAccordion.instances.length;
+
+    JSAccordion.instances.push(this);
+
+    return this;
+}
+
+//  define static property to keep all instances
+JSAccordion.instances = [];
