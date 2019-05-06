5884ca20479dacf6761fa0526e187df094ed7a77
behnaz-hakkak <behnaz.h67@gmail.com>
2019-05-06 12:30:02 +0430

    Initial Changes

---
+++ src/js/jsaccordion.js
@@ -0,0 +1,32 @@
+function JSAccordion(elementOrSelector) {
+    if(!(this instanceof JSAccordion))
+        return new JSAccordion(elementOrSelector);
+
+    this.init = function () {
+
+    };
+
+
+    //  start construction operations
+    //  if parameter is element selector
+    if(typeof elementOrSelector == 'string') {
+        this.targetElement = document.querySelector(elementOrSelector);
+        if(this.targetElement == null) {
+            throw ('invalid element selector');
+        }
+    }
+    //  if parameter is element DOM object
+    else if(typeof elementOrSelector == 'object')
+        this.targetElement = elementOrSelector;
+    else
+        throw ('Unknown element type');
+
+    //  set autoincrement instance id to object
+    this.id = JSAccordion.instances.length;
+
+    JSAccordion.instances.push(this);
+    return this;
+}
+
+//  define static property to keep all instances
+JSAccordion.instances = [];
